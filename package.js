1c336a49ff30ca09ab19bd9239ccbb5627344bc9
GolemClub1994 <dcapalbo165@gmail.com>
2026-04-05 04:13:37 +0200

    Create package.js

---
+++ package.js
@@ -0,0 +1,11 @@
+{
+  "name": "nexus-mine",
+  "version": "10.0.0",
+  "private": true,
+  "description": "NEXUS MINE — TON blockchain mining game",
+  "dependencies": {
+    "@ton/crypto": "^3.2.0",
+    "@ton/ton": "^13.9.0",
+    "@vercel/kv": "^1.0.0"
+  }
+}
